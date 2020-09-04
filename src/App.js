5959db3d09afd6816d830577e3525ddcd67c9916
mahadi <mahadih384@gmail.com>
2020-09-05 00:18:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [factorialNumber, setFactorialNumber] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+
+  const factorial = (n) => {
+    if(n < 0) {
+      return 'Number must be > or = to 0'
+    }
+    if(n == 0) {
+      return 1;
+    }
+    return n * factorial(n-1);
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Factorial Calculator</h1>
+        <form>
+          <input type="number" placeholder="Enter a number..." onChange={handleChange}/>
+          <br /> <br />
+          <button type="button" onClick={() => setFactorialNumber(factorial(inputValue))}>Calculate Factorial</button>
+        </form>
+        <h2>Factorial: {factorialNumber}</h2>
     </div>
   );
 }
